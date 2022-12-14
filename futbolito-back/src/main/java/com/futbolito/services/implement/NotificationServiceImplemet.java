0ba695bd67ce5a30d3b6c91b1ea5def4a5c9e447
Cristobal Cifuentes <cristobal.cifuentes@clave.cl>
2022-12-13 21:48:25 -0300

    optimizacion codigo

--- futbolito-back/src/main/java/com/futbolito/services/implement/NotificationServiceImplemet.java
+++ futbolito-back/src/main/java/com/futbolito/services/implement/NotificationServiceImplemet.java
@@ -1,10 +1,12 @@
 package com.futbolito.services.implement;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
@@ -35,7 +37,7 @@ public class NotificationServiceImplemet implements INotificationService {
 
 	@Autowired
 	private ITypeNotificationRepository typeRep;
-	
+
 	@Autowired
 	private IInvitationRepository invitationRepository;
 
@@ -69,31 +71,17 @@ public class NotificationServiceImplemet implements INotificationService {
 		List<Notification> notifications = notificationRep.findNotificationsbyUserId(idUser);
 		this.changeNotificationStatusToSent(notifications);
 		Map<TypeNotificationEnum, List<Notification>> map = groupNotifications(notifications);
-		List<NotificationDto>  notificationDtos =    findEntitiesRelatedToNotification(map);
+		List<NotificationDto> notificationDtos = findEntitiesRelatedToNotification(map);
 		return notificationDtos;
 	}
 
-	@Async
-	@Override
-	public void createNotification(User userToNotify, Long idReference, TypeNotificationEnum typeNotification) {
-		TypeNotification typeNotifi = typeRep.findTypeNotificationbyType(typeNotification.toString()).orElseThrow();
-		StatusNotification statusNotification = statusRep
-				.findStatusNotificationbyStatus(StatusNotificationEnum.CREATED.name()).orElseThrow();
-		Notification notification = new Notification(userToNotify, statusNotification, typeNotifi, idReference);
-		this.save(notification);
-
-	}
 	
 
 	@Async
 	private void changeNotificationStatusToSent(List<Notification> notifications) {
-
-		List<Long> idNotificationCreated = new ArrayList<>();
-		for (Notification notification : notifications) {
-			if (notification.getStatusNotification().getStatusNotification().equals(StatusNotificationEnum.CREATED)) {
-				idNotificationCreated.add(notification.getIdNotification());
-			}
-		}
+		List<Long> idNotificationCreated = notifications.stream()
+				.filter(n -> n.getStatusNotification().getStatusNotification().equals(StatusNotificationEnum.CREATED))
+				.map(Notification::getIdNotification).collect(Collectors.toList());
 		if (idNotificationCreated.size() > 0) {
 			this.ChangeStatusNotification(idNotificationCreated, StatusNotificationEnum.SENT);
 		}
@@ -103,56 +91,20 @@ public class NotificationServiceImplemet implements INotificationService {
 			StatusNotificationEnum statusNotificationEnum) {
 		notificationRep.updateStatusNotifictions(statusNotificationEnum.name(), idNotificationCreated);
 	}
-	
-	
+
 	private Map<TypeNotificationEnum, List<Notification>> groupNotifications(List<Notification> notifications) {
-		 Map<TypeNotificationEnum, List<Notification>> map = new HashMap<>();
-		 
-		 List<Notification> notificationsTeamInvitation = new ArrayList<Notification>();
-		 List<Notification> notificationsNewTeamChallenge = new ArrayList<Notification>();
-		 
-		 
-		 
-		 
-		 TypeNotificationEnum typeNotification = null;
-		 for(Notification notification: notifications) {
-			 typeNotification = notification.getTypeNotification().getTypeNotification();
-			 if(typeNotification.equals(TypeNotificationEnum.TEAM_INVITATION)) {
-				 notificationsTeamInvitation.add(notification);
-			 } else if(typeNotification.equals(TypeNotificationEnum.NEW_CHALLENGE_TEAM)) {
-				 notificationsNewTeamChallenge.add(notification);
-			 }
-			 
-		 }
-		 
-		 
-		 
-		 
-		 map.put(TypeNotificationEnum.TEAM_INVITATION, notificationsTeamInvitation);
-		 map.put(TypeNotificationEnum.NEW_CHALLENGE_TEAM, notificationsNewTeamChallenge);
-		 
-		
-		
-		
-		
+		Map<TypeNotificationEnum, List<Notification>> map = new HashMap<>();
+		for (Notification notification : notifications) {
+			TypeNotificationEnum type = notification.getTypeNotification().getTypeNotification();
+			if (!map.containsKey(type)) {
+				map.put(type, new ArrayList<Notification>());
+			}
+			map.get(type).add(notification);
+		}
 		return map;
 	}
-	
-	
-	private List<Long> getIdsNotifications(List<Notification> notifications){
-		List<Long> idsNotifications = null; 
-		if(notifications != null && !notifications.isEmpty() && notifications.size() > 0 ) {
-			idsNotifications = new ArrayList<Long>(notifications.size());
-			for(Notification notification : notifications) {
-				idsNotifications.add(notification.getIdReference());
-			}
-		}
-		return idsNotifications;
-	}
-
 
 	private List<NotificationDto> findEntitiesRelatedToNotification(Map<TypeNotificationEnum, List<Notification>> map) {
-
 		List<NotificationDto> notificationDtos = new ArrayList<NotificationDto>();
 		for (Entry<TypeNotificationEnum, List<Notification>> notifications : map.entrySet()) {
 			List<Notification> notificationsList = notifications.getValue();
@@ -160,66 +112,48 @@ public class NotificationServiceImplemet implements INotificationService {
 			Map<Long, Object> entities = searchEntities(idsEntities, notifications.getKey());
 			notificationDtos.addAll(createDtos(notificationsList, entities));
 		}
+		return notificationDtos;
+	}
 
-		return notificationDtos;
+	
+	
+	private Map<Long, Object> searchEntities(List<Long> ids, TypeNotificationEnum typeNotification) {
+	    if (typeNotification.equals(TypeNotificationEnum.TEAM_INVITATION)) {
+	        List<Invitation> entities = invitationRepository.findAllById(ids);
+	        if (entities != null && !entities.isEmpty()) {
+	            return entities.stream()
+	                           .collect(Collectors.toMap(Invitation::getIdInvitation, invitation -> (Object) invitation));
+	        }
+	    } // en un else if agregar la busqueda de diferentes tipos de notificaciones
+	    return Collections.emptyMap();
 	}
-	
-	private Map<Long, Object> searchEntities(List<Long> ids, TypeNotificationEnum typeNotification){
-		Map<Long, Object> objects = new HashMap<Long, Object>();
-		if(typeNotification.equals(TypeNotificationEnum.TEAM_INVITATION)) {
-			List<Invitation> entities =  invitationRepository.findAllById(ids);		
-			if(entities != null && !entities.isEmpty()) {
-				for(Invitation entitie: entities) {
-					objects.put(entitie.getIdInvitation(), (Object) entitie);
-				}
-			}
-		} // en un else if agregar la busqueda de diferentes tipos de notificaciones
-		return objects;
-	}
-	
-	
-	
+
+
 	private List<NotificationDto> createDtos(List<Notification> notifications, Map<Long, Object> entities) {
-		 List<NotificationDto> notificationDtos = new ArrayList<NotificationDto>();
-		 for(Notification notification: notifications) {
-			 notificationDtos.add(new NotificationDto(notification, entities.get(notification.getIdReference())));
-		 }
+		List<NotificationDto> notificationDtos = new ArrayList<NotificationDto>();
+		for (Notification notification : notifications) {
+			notificationDtos.add(new NotificationDto(notification, entities.get(notification.getIdReference())));
+		}
 		return notificationDtos;
 	}
+
+	private List<Long> getIdsNotifications(List<Notification> notifications) {
+
+		if (notifications == null || notifications.isEmpty()) {
+			return null;
+		}
+		return notifications.stream().map(notification -> notification.getIdReference()).collect(Collectors.toList());
+	}
+
 	
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	@Async
+	@Override
+	public void createNotification(User userToNotify, Long idReference, TypeNotificationEnum typeNotification) {
+		TypeNotification typeNotifi = typeRep.findTypeNotificationbyType(typeNotification.toString()).orElseThrow();
+		StatusNotification statusNotification = statusRep
+				.findStatusNotificationbyStatus(StatusNotificationEnum.CREATED.name()).orElseThrow();
+		Notification notification = new Notification(userToNotify, statusNotification, typeNotifi, idReference);
+		this.save(notification);
+	}
 }
