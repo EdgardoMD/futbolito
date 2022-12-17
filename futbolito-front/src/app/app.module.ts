e3ae32d527bd91043c8068e1cf5c3597bff3940c
Cristobal Cifuentes <cristobal.cifuentes@clave.cl>
2022-12-17 02:40:55 -0300

    se migra a angular 15 y se incluye angular material

--- futbolito-front/src/app/app.module.ts
+++ futbolito-front/src/app/app.module.ts
@@ -1,17 +1,17 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule} from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PerfilModule } from './modules/perfil/perfil.module';
 import { HomeComponent } from './modules/home/componets/home/home.component';
-import { FormsModule } from '@angular/forms';
 import { interceptorProvider } from './interceptors/token-intepcetor.service';
 import { TeamsModule } from './modules/teams/teams.module';
 import { HeaderModule } from './modules/header/header.module';
 import { NotificationsModule } from './modules/notifications/notifications.module';
 import { AutenticacionModule } from './modules/autenticacion/autenticacion.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
 
 @NgModule({
   declarations: [
@@ -24,10 +24,10 @@ import { AutenticacionModule } from './modules/autenticacion/autenticacion.modul
     AppRoutingModule,
     PerfilModule,
     AutenticacionModule,
-    FormsModule,
     TeamsModule,
     HeaderModule,
-    NotificationsModule
+    NotificationsModule,
+    BrowserAnimationsModule
   ],
   providers: [interceptorProvider],
   bootstrap: [AppComponent]
