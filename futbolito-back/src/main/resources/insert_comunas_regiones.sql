05600b7f527d1ee77c421523e330d515f765f57d
Cristobal Cifuentes <c.cifuentes1990@gmail.com>
2023-01-10 23:56:38 -0300

    insert comunas regiones

---
+++ futbolito-back/src/main/resources/insert_comunas_regiones.sql
@@ -0,0 +1,321 @@
+----------------------------------
+-----------REGIONES---------
+----------------------------------
+
+INSERT INTO regions (id_region, region) VALUES(1, 'O´Higgins');
+INSERT INTO regions (id_region, region) VALUES(2, 'Metropolitana');
+INSERT INTO regions (id_region, region) VALUES(3, 'Valparaíso');
+INSERT INTO regions (id_region, region) VALUES(4, 'Biobío');
+INSERT INTO regions (id_region, region) VALUES(5, 'Maule');
+INSERT INTO regions (id_region, region) VALUES(6, 'La Araucanía');
+
+
+
+
+
+----------------------------------
+--------CIUDADES------------------
+----------------------------------
+
+--------------OHIGGINS--------------------
+INSERT INTO citys (id_city, city, id_region) VALUES (1, 'Rancagua', 1);
+INSERT INTO citys (id_city, city, id_region) VALUES (2, 'San Fernando', 1);
+INSERT INTO citys (id_city, city, id_region) VALUES (3, 'Rengo', 1);
+INSERT INTO citys (id_city, city, id_region) VALUES (4, 'Machalí', 1);
+INSERT INTO citys (id_city, city, id_region) VALUES (5, 'San Vicente', 1);
+INSERT INTO citys (id_city, city, id_region) VALUES (6, 'Santa Cruz', 1);
+INSERT INTO citys (id_city, city, id_region) VALUES (7, 'Chimbarongo', 1);
+INSERT INTO citys (id_city, city, id_region) VALUES (8, 'Graneros', 1);
+INSERT INTO citys (id_city, city, id_region) VALUES (9, 'Requínoa', 1);
+INSERT INTO citys (id_city, city, id_region) VALUES (10, 'Mostazal', 1);
+INSERT INTO citys (id_city, city, id_region) VALUES (11, 'Las Cabras', 1);
+INSERT INTO citys (id_city, city, id_region) VALUES (12, 'Doñihue', 1);
+INSERT INTO citys (id_city, city, id_region) VALUES (13, 'Coltauco', 1);
+INSERT INTO citys (id_city, city, id_region) VALUES (14, 'Pichidegua', 1);
+INSERT INTO citys (id_city, city, id_region) VALUES (15, 'Nancagua', 1);
+INSERT INTO citys (id_city, city, id_region) VALUES (16, 'Pichilemu', 1);
+INSERT INTO citys (id_city, city, id_region) VALUES (17, 'Chépica', 1);
+INSERT INTO citys (id_city, city, id_region) VALUES (18, 'Peumo', 1);
+INSERT INTO citys (id_city, city, id_region) VALUES (19, 'Olivar', 1);
+INSERT INTO citys (id_city, city, id_region) VALUES (20, 'Malloa', 1);
+INSERT INTO citys (id_city, city, id_region) VALUES (21, 'Codegua', 1);
+INSERT INTO citys (id_city, city, id_region) VALUES (22, 'Quinta de Tilcoco', 1);
+INSERT INTO citys (id_city, city, id_region) VALUES (23, 'Palmilla', 1);
+INSERT INTO citys (id_city, city, id_region) VALUES (24, 'Peralillo', 1);
+INSERT INTO citys (id_city, city, id_region) VALUES (25, 'Placilla', 1);
+INSERT INTO citys (id_city, city, id_region) VALUES (26, 'Coinco', 1);
+INSERT INTO citys (id_city, city, id_region) VALUES (27, 'Marchihue', 1);
+INSERT INTO citys (id_city, city, id_region) VALUES (28, 'Lolol', 1);
+INSERT INTO citys (id_city, city, id_region) VALUES (29, 'Navidad', 1);
+INSERT INTO citys (id_city, city, id_region) VALUES (30, 'Litueche', 1);
+INSERT INTO citys (id_city, city, id_region) VALUES (31, 'Paredones', 1);
+INSERT INTO citys (id_city, city, id_region) VALUES (32, 'Pumanque', 1);
+INSERT INTO citys (id_city, city, id_region) VALUES (33, 'La Estrella', 1);
+
+--------------METROPOLITANA--------------------
+
+INSERT INTO citys (id_city, city, id_region) VALUES (34, 'Puente Alto', 2);
+INSERT INTO citys (id_city, city, id_region) VALUES (35, 'Maipú', 2);
+INSERT INTO citys (id_city, city, id_region) VALUES (36, 'Santiago', 2);
+INSERT INTO citys (id_city, city, id_region) VALUES (37, 'La Florida', 2);
+INSERT INTO citys (id_city, city, id_region) VALUES (38, 'San Bernardo', 2);
+INSERT INTO citys (id_city, city, id_region) VALUES (39, 'Las Condes', 2);
+INSERT INTO citys (id_city, city, id_region) VALUES (40, 'Peñalolén', 2);
+INSERT INTO citys (id_city, city, id_region) VALUES (41, 'Pudahuel', 2);
+INSERT INTO citys (id_city, city, id_region) VALUES (42, 'Quilicura', 2);
+INSERT INTO citys (id_city, city, id_region) VALUES (43, 'Ñuñoa', 2);
+INSERT INTO citys (id_city, city, id_region) VALUES (44, 'La Pintana', 2);
+INSERT INTO citys (id_city, city, id_region) VALUES (45, 'El Bosque', 2);
+INSERT INTO citys (id_city, city, id_region) VALUES (46, 'Recoleta', 2);
+INSERT INTO citys (id_city, city, id_region) VALUES (47, 'Renca', 2);
+INSERT INTO citys (id_city, city, id_region) VALUES (48, 'Estación Central', 2);
+INSERT INTO citys (id_city, city, id_region) VALUES (49, 'Colina', 2);
+INSERT INTO citys (id_city, city, id_region) VALUES (50, 'Providencia', 2);
+INSERT INTO citys (id_city, city, id_region) VALUES (51, 'Cerro Navia', 2);
+INSERT INTO citys (id_city, city, id_region) VALUES (52, 'Conchalí', 2);
+INSERT INTO citys (id_city, city, id_region) VALUES (53, 'Melipilla', 2);
+INSERT INTO citys (id_city, city, id_region) VALUES (54, 'La Granja', 2);
+INSERT INTO citys (id_city, city, id_region) VALUES (55, 'Macul', 2);
+INSERT INTO citys (id_city, city, id_region) VALUES (56, 'Quinta Normal', 2);
+INSERT INTO citys (id_city, city, id_region) VALUES (57, 'San Miguel', 2);
+INSERT INTO citys (id_city, city, id_region) VALUES (58, 'Lo Barnechea', 2);
+INSERT INTO citys (id_city, city, id_region) VALUES (59, 'Lampa', 2);
+INSERT INTO citys (id_city, city, id_region) VALUES (60, 'Pedro Aguirre Cerda', 2);
+INSERT INTO citys (id_city, city, id_region) VALUES (61, 'Independencia', 2);
+INSERT INTO citys (id_city, city, id_region) VALUES (62, 'Lo Espejo', 2);
+INSERT INTO citys (id_city, city, id_region) VALUES (63, 'Huechuraba', 2);
+INSERT INTO citys (id_city, city, id_region) VALUES (64, 'Buin', 2);
+INSERT INTO citys (id_city, city, id_region) VALUES (65, 'Lo Prado', 2);
+INSERT INTO citys (id_city, city, id_region) VALUES (66, 'San Joaquín', 2);
+INSERT INTO citys (id_city, city, id_region) VALUES (67, 'La Reina', 2);
+INSERT INTO citys (id_city, city, id_region) VALUES (68, 'Peñaflor', 2);
+INSERT INTO citys (id_city, city, id_region) VALUES (69, 'La Cisterna', 2);
+INSERT INTO citys (id_city, city, id_region) VALUES (70, 'Vitacura', 2);
+INSERT INTO citys (id_city, city, id_region) VALUES (71, 'San Ramón', 2);
+INSERT INTO citys (id_city, city, id_region) VALUES (72, 'Cerrillos', 2);
+INSERT INTO citys (id_city, city, id_region) VALUES (73, 'Talagante', 2);
+INSERT INTO citys (id_city, city, id_region) VALUES (74, 'Paine', 2);
+INSERT INTO citys (id_city, city, id_region) VALUES (75, 'Padre Hurtado', 2);
+INSERT INTO citys (id_city, city, id_region) VALUES (76, 'Isla de Maipo', 2);
+INSERT INTO citys (id_city, city, id_region) VALUES (77, 'El Monte', 2);
+INSERT INTO citys (id_city, city, id_region) VALUES (78, 'Curacaví', 2);
+INSERT INTO citys (id_city, city, id_region) VALUES (79, 'Pirque', 2);
+INSERT INTO citys (id_city, city, id_region) VALUES (80, 'Calera de Tango', 2);
+INSERT INTO citys (id_city, city, id_region) VALUES (81, 'Tiltil', 2);
+INSERT INTO citys (id_city, city, id_region) VALUES (82, 'San José de Maipo', 2);
+INSERT INTO citys (id_city, city, id_region) VALUES (83, 'María Pinto', 2);
+INSERT INTO citys (id_city, city, id_region) VALUES (84, 'San Pedro', 2);
+INSERT INTO citys (id_city, city, id_region) VALUES (85, 'Alhué', 2);
+
+
+--------------VALPARAISO--------------------
+
+INSERT INTO citys (id_city, city, id_region) VALUES (86, 'Viña del Mar', 3);
+INSERT INTO citys (id_city, city, id_region) VALUES (87, 'Valparaíso', 3);
+INSERT INTO citys (id_city, city, id_region) VALUES (88, 'Quilpué', 3);
+INSERT INTO citys (id_city, city, id_region) VALUES (89, 'Villa Alemana', 3);
+INSERT INTO citys (id_city, city, id_region) VALUES (90, 'San Antonio', 3);
+INSERT INTO citys (id_city, city, id_region) VALUES (91, 'Quillota', 3);
+INSERT INTO citys (id_city, city, id_region) VALUES (92, 'San Felipe', 3);
+INSERT INTO citys (id_city, city, id_region) VALUES (93, 'Los Andes', 3);
+INSERT INTO citys (id_city, city, id_region) VALUES (94, 'La Calera', 3);
+INSERT INTO citys (id_city, city, id_region) VALUES (95, 'Limache', 3);
+INSERT INTO citys (id_city, city, id_region) VALUES (96, 'Concón', 3);
+INSERT INTO citys (id_city, city, id_region) VALUES (97, 'La Ligua', 3);
+INSERT INTO citys (id_city, city, id_region) VALUES (98, 'Quintero', 3);
+INSERT INTO citys (id_city, city, id_region) VALUES (99, 'Casablanca', 3);
+INSERT INTO citys (id_city, city, id_region) VALUES (100, 'Llaillay', 3);
+INSERT INTO citys (id_city, city, id_region) VALUES (101, 'Cartagena', 3);
+INSERT INTO citys (id_city, city, id_region) VALUES (102, 'Nogales', 3);
+INSERT INTO citys (id_city, city, id_region) VALUES (103, 'La Cruz', 3);
+INSERT INTO citys (id_city, city, id_region) VALUES (104, 'Cabildo', 3);
+INSERT INTO citys (id_city, city, id_region) VALUES (105, 'San Esteban', 3);
+INSERT INTO citys (id_city, city, id_region) VALUES (106, 'Puchuncaví', 3);
+INSERT INTO citys (id_city, city, id_region) VALUES (107, 'Hijuelas', 3);
+INSERT INTO citys (id_city, city, id_region) VALUES (108, 'Olmué', 3);
+INSERT INTO citys (id_city, city, id_region) VALUES (109, 'Putaendo', 3);
+INSERT INTO citys (id_city, city, id_region) VALUES (110, 'El Quisco', 3);
+INSERT INTO citys (id_city, city, id_region) VALUES (111, 'Santa María', 3);
+INSERT INTO citys (id_city, city, id_region) VALUES (112, 'Calle Larga', 3);
+INSERT INTO citys (id_city, city, id_region) VALUES (113, 'Catemu', 3);
+INSERT INTO citys (id_city, city, id_region) VALUES (114, 'Algarrobo', 3);
+INSERT INTO citys (id_city, city, id_region) VALUES (115, 'El Tabo', 3);
+INSERT into citys (id_city, city, id_region) VALUES (116, 'Santo Domingo', 3);
+INSERT INTO citys (id_city, city, id_region) VALUES (117, 'Rinconada', 3);
+INSERT INTO citys (id_city, city, id_region) VALUES (118, 'Petorca', 3);
+INSERT INTO citys (id_city, city, id_region) VALUES (119, 'Isla de Pascua', 3);
+INSERT INTO citys (id_city, city, id_region) VALUES (120, 'Zapallar', 3);
+INSERT INTO citys (id_city, city, id_region) VALUES (121, 'Panquehue', 3);
+INSERT INTO citys (id_city, city, id_region) VALUES (122, 'Papudo', 3);
+INSERT INTO citys (id_city, city, id_region) VALUES (123, 'Juan Fernández', 3);
+
+--------------bio bio--------------------
+
+INSERT INTO citys (id_city, city, id_region) VALUES (124, 'Concepción', 4);
+INSERT INTO citys (id_city, city, id_region) VALUES (125, 'Los Ángeles', 4);
+INSERT INTO citys (id_city, city, id_region) VALUES (126, 'Talcahuano', 4);
+INSERT INTO citys (id_city, city, id_region) VALUES (127, 'San Pedro de la Paz', 4);
+INSERT INTO citys (id_city, city, id_region) VALUES (128, 'Coronel', 4);
+INSERT INTO citys (id_city, city, id_region) VALUES (129, 'Hualpén', 4);
+INSERT INTO citys (id_city, city, id_region) VALUES (130, 'Chiguayante', 4);
+INSERT INTO citys (id_city, city, id_region) VALUES (131, 'Tomé', 4);
+INSERT INTO citys (id_city, city, id_region) VALUES (132, 'Penco', 4);
+INSERT INTO citys (id_city, city, id_region) VALUES (133, 'Lota', 4);
+INSERT INTO citys (id_city, city, id_region) VALUES (134, 'Arauco', 4);
+INSERT INTO citys (id_city, city, id_region) VALUES (135, 'Cañete', 4);
+INSERT INTO citys (id_city, city, id_region) VALUES (136, 'Curanilahue', 4);
+INSERT INTO citys (id_city, city, id_region) VALUES (137, 'Mulchén', 4);
+INSERT INTO citys (id_city, city, id_region) VALUES (138, 'Cabrero', 4);
+INSERT INTO citys (id_city, city, id_region) VALUES (139, 'Nacimiento', 4);
+INSERT INTO citys (id_city, city, id_region) VALUES (140, 'Lebu', 4);
+INSERT INTO citys (id_city, city, id_region) VALUES (141, 'Hualqui', 4);
+INSERT INTO citys (id_city, city, id_region) VALUES (142, 'Laja', 4);
+INSERT INTO citys (id_city, city, id_region) VALUES (143, 'Yumbel', 4);
+INSERT INTO citys (id_city, city, id_region) VALUES (144, 'Los Álamos', 4);
+INSERT INTO citys (id_city, city, id_region) VALUES (145, 'Tucapel', 4);
+INSERT INTO citys (id_city, city, id_region) VALUES (146, 'Santa Bárbara', 4);
+INSERT INTO citys (id_city, city, id_region) VALUES (147, 'Santa Juana', 4);
+INSERT INTO citys (id_city, city, id_region) VALUES (148, 'Florida', 4);
+INSERT INTO citys (id_city, city, id_region) VALUES (149, 'Tirúa', 4);
+INSERT INTO citys (id_city, city, id_region) VALUES (150, 'Negrete', 4);
+INSERT INTO citys (id_city, city, id_region) VALUES (151, 'Quilleco', 4);
+INSERT INTO citys (id_city, city, id_region) VALUES (152, 'Contulmo', 4);
+INSERT INTO citys (id_city, city, id_region) VALUES (153, 'Alto Biobío', 4);
+INSERT INTO citys (id_city, city, id_region) VALUES (154, 'Antuco', 4);
+INSERT INTO citys (id_city, city, id_region) VALUES (155, 'Quilaco', 4);
+INSERT INTO citys (id_city, city, id_region) VALUES (156, 'San Rosendo', 4);
+
+
+--------------bio bio--------------------
+
+INSERT INTO citys (id_city, city, id_region) VALUES (157, 'Talca', 5);
+INSERT INTO citys (id_city, city, id_region) VALUES (158, 'Curicó', 5);
+INSERT INTO citys (id_city, city, id_region) VALUES (159, 'Linares', 5);
+INSERT INTO citys (id_city, city, id_region) VALUES (160, 'Maule', 5);
+INSERT INTO citys (id_city, city, id_region) VALUES (161, 'Constitución', 5);
+INSERT INTO citys (id_city, city, id_region) VALUES (162, 'Molina', 5);
+INSERT INTO citys (id_city, city, id_region) VALUES (163, 'San Javier', 5);
+INSERT INTO citys (id_city, city, id_region) VALUES (164, 'San Clemente', 5);
+INSERT INTO citys (id_city, city, id_region) VALUES (165, 'Parral', 5);
+INSERT INTO citys (id_city, city, id_region) VALUES (166, 'Cauquenes', 5);
+INSERT INTO citys (id_city, city, id_region) VALUES (167, 'Longaví', 5);
+INSERT INTO citys (id_city, city, id_region) VALUES (168, 'Teno', 5);
+INSERT INTO citys (id_city, city, id_region) VALUES (169, 'Colbún', 5);
+INSERT INTO citys (id_city, city, id_region) VALUES (170, 'Retiro', 5);
+INSERT INTO citys (id_city, city, id_region) VALUES (171, 'Sagrada Familia', 5);
+INSERT INTO citys (id_city, city, id_region) VALUES (172, 'Yerbas Buenas', 5);
+INSERT INTO citys (id_city, city, id_region) VALUES (173, 'Villa Alegre', 5);
+INSERT INTO citys (id_city, city, id_region) VALUES (174, 'Romeral', 5);
+INSERT INTO citys (id_city, city, id_region) VALUES (175, 'Río Claro', 5);
+INSERT INTO citys (id_city, city, id_region) VALUES (176, 'Rauco', 5);
+INSERT INTO citys (id_city, city, id_region) VALUES (177, 'Hualañé', 5);
+INSERT INTO citys (id_city, city, id_region) VALUES (178, 'Curepto', 5);
+INSERT INTO citys (id_city, city, id_region) VALUES (179, 'San Rafael', 5);
+INSERT INTO citys (id_city, city, id_region) VALUES (180, 'Chanco', 5);
+INSERT INTO citys (id_city, city, id_region) VALUES (181, 'Pelarco', 5);
+INSERT INTO citys (id_city, city, id_region) VALUES (182, 'Pencahue', 5);
+INSERT INTO citys (id_city, city, id_region) VALUES (183, 'Pelluhue', 5);
+INSERT INTO citys (id_city, city, id_region) VALUES (184, 'Licantén', 5);
+INSERT INTO citys (id_city, city, id_region) VALUES (185, 'Vichuquén', 5);
+INSERT INTO citys (id_city, city, id_region) VALUES (186, 'Empedrado', 5);
+
+--------------MAULE--------------------
+
+INSERT INTO citys (id_city, city, id_region) VALUES (187, 'Temuco', 6);
+INSERT INTO citys (id_city, city, id_region) VALUES (188, 'Padre Las Casas', 6);
+INSERT INTO citys (id_city, city, id_region) VALUES (189, 'Villarrica', 6);
+INSERT INTO citys (id_city, city, id_region) VALUES (190, 'Angol', 6);
+INSERT INTO citys (id_city, city, id_region) VALUES (191, 'Lautaro', 6);
+INSERT INTO citys (id_city, city, id_region) VALUES (192, 'Victoria', 6);
+INSERT INTO citys (id_city, city, id_region) VALUES (193, 'Nueva Imperial', 6);
+INSERT INTO citys (id_city, city, id_region) VALUES (194, 'Pucón', 6);
+INSERT INTO citys (id_city, city, id_region) VALUES (195, 'Vilcún', 6);
+INSERT INTO citys (id_city, city, id_region) VALUES (196, 'Pitrufquén', 6);
+INSERT INTO citys (id_city, city, id_region) VALUES (197, 'Freire', 6);
+INSERT INTO citys (id_city, city, id_region) VALUES (198, 'Collipulli', 6);
+INSERT INTO citys (id_city, city, id_region) VALUES (199, 'Carahue', 6);
+INSERT INTO citys (id_city, city, id_region) VALUES (200, 'Loncoche', 6);
+INSERT INTO citys (id_city, city, id_region) VALUES (201, 'Traiguén', 6);
+INSERT INTO citys (id_city, city, id_region) VALUES (202, 'Cunco', 6);
+INSERT INTO citys (id_city, city, id_region) VALUES (203, 'Curacautín', 6);
+INSERT INTO citys (id_city, city, id_region) VALUES (204, 'Teodoro Schmidt', 6);
+INSERT INTO citys (id_city, city, id_region) VALUES (205, 'Gorbea', 6);
+INSERT INTO citys (id_city, city, id_region) VALUES (206, 'Saavedra', 6);
+INSERT INTO citys (id_city, city, id_region) VALUES (207, 'Galvarino', 6);
+INSERT INTO citys (id_city, city, id_region) VALUES (208, 'Purén', 6);
+INSERT INTO citys (id_city, city, id_region) VALUES (209, 'Cholchol', 6);
+INSERT INTO citys (id_city, city, id_region) VALUES (210, 'Lonquimay', 6);
+INSERT INTO citys (id_city, city, id_region) VALUES (211, 'Renaico', 6);
+INSERT INTO citys (id_city, city, id_region) VALUES (212, 'Toltén', 6);
+INSERT INTO citys (id_city, city, id_region) VALUES (213, 'Lumaco', 6);
+INSERT INTO citys (id_city, city, id_region) VALUES (214, 'Ercilla', 6);
+INSERT INTO citys (id_city, city, id_region) VALUES (215, 'Curarrehue', 6);
+INSERT INTO citys (id_city, city, id_region) VALUES (216, 'Los Sauces', 6);
+INSERT INTO citys (id_city, city, id_region) VALUES (217, 'Perquenco', 6);
+INSERT INTO citys (id_city, city, id_region) VALUES (218, 'Melipeuco', 6);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+select Count(c.id_city), r.region  from citys c 
+join regions r  on r.id_region = c.id_region  group by c.id_region, r.region ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
